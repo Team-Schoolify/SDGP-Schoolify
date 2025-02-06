97c1da4467fb6e066fba280dbb25b0a0369bac6c
Hasaru Uyanahewa <hasaruuyanahewa08@gmail.com>
2025-02-06 21:30:16 +0530

    Fixed line endings and re-added school-calendar

---
+++ Schoolify Calendar/school-calendar/app/page.js
@@ -0,0 +1,10 @@
+import EventsDrawer from "./components/EventsDrawer";
+//import SchoolCalendar from "./components/SchoolCalendar";
+
+export default function Home() {
+  return (
+    <>
+      <EventsDrawer/>
+    </>
+  );
+}
