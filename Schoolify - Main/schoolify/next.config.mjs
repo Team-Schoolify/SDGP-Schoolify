6377f16548229d08db777492c7c53b5aa9aa7931
SonalKethaka <sonalpathinayaka7@gmail.com>
2025-02-09 02:34:54 +0530

    Delete Schoolify - Main directory

--- Schoolify - Main/schoolify/next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        domains: ['nextui.org'], // Add any other domains here
-    },
-};
-
-export default nextConfig;
