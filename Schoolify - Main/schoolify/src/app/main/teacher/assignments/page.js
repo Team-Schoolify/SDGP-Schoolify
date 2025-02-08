8a237a4fa1125ca2a07ca15860838f44181e5ce3
SonalPathinayaka <sonalpathinayaka7@gmail.com>
2025-02-09 00:34:28 +0530

    JJJJJ

---
+++ Schoolify - Main/schoolify/src/app/main/teacher/assignments/page.js
@@ -0,0 +1,14 @@
+"use client";
+
+import React from "react";
+
+export default function Page() {
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-gray-100">
+            <h1 className="text-4xl font-bold text-gray-800">
+                Welcome to Assignments Page of Teachers!
+            </h1>
+
+        </div>
+    );
+}
