ad430701d7ec1f91a818fdbf181703734dd44648
SathiraHD <hdsathira@gmail.com>
2025-02-18 17:57:00 +0530

    Add resources page layout and implement gradebook component

--- Schoolify - Main/schoolify/src/app/main/student/resources/page.js
+++ Schoolify - Main/schoolify/src/app/main/student/resources/page.js
@@ -5,10 +5,16 @@ import React from "react";
 export default function Page() {
     return (
         <div className="min-h-screen flex items-center justify-center bg-gray-100">
-            <h1 className="text-4xl font-bold text-gray-800">
-                Welcome to Resources Page of Students!
-            </h1>
-
+            <h1 className="absolute top-36 left-52 text-5xl font-bold text-gray-800">Science</h1>
+            <div className="w-full max-w-screen-lg h-auto  bg-[#87CBFF] shadow-xl rounded-lg p-4 mt-4 absolute top-60 left-48">
+                <h2 className="text-left text-lg font-bold text-gray-800">Content goes here.</h2>
+            </div>
+            <div className="w-full max-w-screen-lg h-auto  bg-[#87CBFF] shadow-xl rounded-lg p-4 mt-4 absolute slide-in-from-top-72 left-48">
+                <h2 className="text-left text-lg font-bold text-gray-800">Content goes here.</h2>
+            </div>
+            <div className="w-full max-w-screen-lg h-auto  bg-[#87CBFF] shadow-xl rounded-lg p-4 mt-4 absolute top-3/4 left-48">
+                <h2 className="text-left text-lg font-bold text-gray-800">Content goes here.</h2>
+            </div>
         </div>
     );
 }
