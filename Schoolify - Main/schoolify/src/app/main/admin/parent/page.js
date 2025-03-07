784a88b906fa7144bd29b6a84623c836fe909a0a
SonalPathinayaka <sonalpathinayaka7@gmail.com>
2025-03-07 13:37:53 +0530

    Finished every form sections in the admin pages

--- Schoolify - Main/schoolify/src/app/main/admin/parent/page.js
+++ Schoolify - Main/schoolify/src/app/main/admin/parent/page.js
@@ -1,11 +1,12 @@
 "use client";
 import React from 'react';
+import AdminParentForm from "@/app/main/admin/parent/AdminParentForm";
 
 export default function LoginPage() {
 
     return (
         <div className="min-h-screen">
-            Admin Parent
+            <AdminParentForm/>
         </div>
     );
 }
