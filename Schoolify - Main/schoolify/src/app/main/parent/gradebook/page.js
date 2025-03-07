978c0bd0af656ecf39396e1001c4b2c64397674e
SachiniNishshanka <sachini.20230851@iit.ac.lk>
2025-03-07 23:35:50 +0530

    Update page.js

--- Schoolify - Main/schoolify/src/app/main/parent/gradebook/page.js
+++ Schoolify - Main/schoolify/src/app/main/parent/gradebook/page.js
@@ -7,3 +7,17 @@ import { Card } from "@heroui/card";
 export default function GradebookPage() {
     const router = useRouter();
     const [terms, setTerms] = useState([]);
+
+    // Fetch available years & terms from API
+  useEffect(() => {
+    const fetchTerms = async () => {
+      try {
+        const response = await fetch("/api/terms");
+        const data = await response.json();
+        setTerms(data);
+      } catch (error) {
+        console.error("Error fetching terms:", error);
+      }
+    };
+    fetchTerms();
+  }, []);
