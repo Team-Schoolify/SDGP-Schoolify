b0e5d4151aa7eeabc47d986599cb98bc1bacd740
SachiniNishshanka <sachini.20230851@iit.ac.lk>
2025-03-09 00:51:11 +0530

    Update student gradebook

--- Schoolify - Main/schoolify/src/app/main/student/gradebook/page.js
+++ Schoolify - Main/schoolify/src/app/main/student/gradebook/page.js
@@ -2,3 +2,20 @@
 
 import React from "react";
 import { Card, CardHeader, CardBody, CardFooter } from "@heroui/card";
+
+export default function GradebookPage() {
+    const reportData = [
+      {
+        year: "2023",
+        studentName: "John Doe",
+        grade: "10",
+        subjects: [
+          { name: "Maths", score: 75 },
+          { name: "Science", score: 68 },
+          { name: "English Language", score: 80 },
+          { name: "Tamil", score: 76 },
+          { name: "History", score: 50 },
+          { name: "Western Music", score: 68 },
+          { name: "ICT", score: 90 },
+        ],
+      },
