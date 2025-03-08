79db1cbe311c7293b013a4ceb9de8ecc5ace38d6
SachiniNishshanka <sachini.20230851@iit.ac.lk>
2025-03-09 00:43:17 +0530

    Update student gradebook

--- Schoolify - Main/schoolify/src/app/main/student/gradebook/page.js
+++ Schoolify - Main/schoolify/src/app/main/student/gradebook/page.js
@@ -1,12 +1,4 @@
 "use client";
 
 import React from "react";
-import ResultSheet from "@/app/main/student/gradebook/ResultSheet";
-
-export default function Page() {
-    return (
-        <div className="min-h-screen">
-            <ResultSheet/>
-        </div>
-    );
-}
+import { Card, CardHeader, CardBody, CardFooter } from "@heroui/card";
