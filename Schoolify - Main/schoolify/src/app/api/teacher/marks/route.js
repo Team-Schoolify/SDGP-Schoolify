224d57a98fb44d32980d5746c44bfd1ae8e5b14d
SachiniNishshanka <sachini.20230851@iit.ac.lk>
2025-03-09 17:04:08 +0530

    Update route.js

--- Schoolify - Main/schoolify/src/app/api/teacher/marks/route.js
+++ Schoolify - Main/schoolify/src/app/api/teacher/marks/route.js
@@ -5,6 +5,16 @@ let teacherMarks = [
   { year: 2023, term: 1, subject: "Maths", studentName: "John Doe", marks: 75 },
 ];
 
+// Fetch marks for teachers (based on selected year & term)
+export async function GET(req) {
+  const { searchParams } = new URL(req.url);
+  const year = searchParams.get("year");
+  const term = searchParams.get("term");
+  const subject = searchParams.get("subject");
+
+
+
+
 
   
   
