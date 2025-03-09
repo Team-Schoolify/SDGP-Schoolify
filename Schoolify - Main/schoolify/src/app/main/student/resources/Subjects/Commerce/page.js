58036ade22e7f3152a63ae2a3b704905c0970fd7
SathiraHD <hdsathira@gmail.com>
2025-03-09 22:22:26 +0530

    connect the commerce card with recources page

---
+++ Schoolify - Main/schoolify/src/app/main/student/resources/Subjects/Commerce/page.js
@@ -0,0 +1,11 @@
+"use client";
+import Commercepage from "@/app/main/student/resources/Subjects/Commerce/Commercepage";
+
+
+export default function LoginPage() {
+    return (
+        <div>
+            <Commercepage/>
+        </div>
+    );
+}
---
+++ Schoolify - Main/schoolify/src/app/main/student/resources/Subjects/ICT/page.js
@@ -0,0 +1,11 @@
+"use client";
+import Sciencepage from "@/app/main/student/resources/Subjects/Science/Sciencepage";
+
+
+export default function LoginPage() {
+    return (
+        <div>
+            <Sciencepage/>
+        </div>
+    );
+}
