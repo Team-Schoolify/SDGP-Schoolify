670685c4bc0ad5ad2d487691683685b8e0c6965a
Chalitha Gunarathna <chalithagunarathna2001@gmail.com>
2025-03-09 15:10:29 +0530

    Deleted unneccessory imports from profileStudent.jsx

--- Schoolify - Main/schoolify/src/app/main/student/profileStudent.jsx
+++ Schoolify - Main/schoolify/src/app/main/student/profileStudent.jsx
@@ -1,14 +1,8 @@
 "use client";
 import React, {useEffect, useState} from 'react'
 import {
-    Badge,
-    Button,
     Avatar,
-    Drawer,
-    DrawerContent,
-    DrawerHeader,
-    DrawerFooter,
-    DrawerBody, useDisclosure,
+    useDisclosure,
     Popover, PopoverTrigger, PopoverContent,
 } from "@heroui/react";
 import {supabase} from "@/app/lib/supabaseClient";
@@ -121,36 +115,6 @@ export const ProfileStudent = () => {
                 </PopoverContent>
             </Popover>
         </div>
-
-        {/*<Drawer isOpen={isOpen} size="sm" onClose={onClose}>*/}
-        {/*    <DrawerContent>*/}
-        {/*        {(onClose) => (*/}
-        {/*            <>*/}
-        {/*                <DrawerHeader className="flex flex-col gap-1">Drawer Title</DrawerHeader>*/}
-        {/*                <DrawerBody>*/}
-        {/*                    <p>*/}
-        {/*                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam pulvinar risus non*/}
-        {/*                        risus hendrerit venenatis. Pellentesque sit amet hendrerit risus, sed porttitor*/}
-        {/*                        quam.*/}
-        {/*                    </p>*/}
-        {/*                    <p>*/}
-        {/*                        Magna exercitation reprehenderit magna aute tempor cupidatat consequat elit dolor*/}
-        {/*                        adipisicing. Mollit dolor eiusmod sunt ex incididunt cillum quis. Velit duis sit*/}
-        {/*                        officia eiusmod Lorem aliqua enim laboris do dolor eiusmod.*/}
-        {/*                    </p>*/}
-        {/*                </DrawerBody>*/}
-        {/*                <DrawerFooter>*/}
-        {/*                    <Button color="danger" variant="light" onPress={onClose}>*/}
-        {/*                        Close*/}
-        {/*                    </Button>*/}
-        {/*                    <Button color="primary" onPress={onClose}>*/}
-        {/*                        Action*/}
-        {/*                    </Button>*/}
-        {/*                </DrawerFooter>*/}
-        {/*            </>*/}
-        {/*        )}*/}
-        {/*    </DrawerContent>*/}
-        {/*</Drawer>*/}
     </>
     )
 }
